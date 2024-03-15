98f95c58de5f747dd392a1a57ea7ac86ea4df701
Zorro <152538076+ThembaJNcube@users.noreply.github.com>
2024-03-15 10:48:21 +0000

    Home page laid out and defined

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./components/Navbar";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Home />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import H1 from "../images/Home/H1.jpg";
+import H2 from "../images/Home/H2.jpg";
 
 function Home() {
   return (
-    <div className="Home-container">
+    <div className="home-container">
       <div className="home-content">
-        <p>Lorem Ipsum 5</p>
+        <img src={H1}></img>
+      </div>
+      <div className="home-content writing">
+        <h1 className="text-2xl italic font-bold">The Dawn of NEO-AGE</h1>
+        <p>Be part of the movement and engage in the future of training.</p>
+      </div>
+      <div className="home-content writing">
+        <h1 className="text-3xl italic font-bold text-[#770707]">NEO-GYM !</h1>
+        <p>
+          is tailored to accommodate everyone and all their needs, and it is
+          here to takeover, avoid being left out and be part of this{" "}
+          <span className="text-xl text-[#770707]">MOVEMENT</span>.
+        </p>
       </div>
       <div className="home-content">
-        <img src=""></img>
-      </div>
-      <div className="home-content">
-        <p>Lorem Ipsum 5</p>
-      </div>
-      <div className="home-content">
-        <img src=""></img>
+        <img src={H2}></img>
       </div>
     </div>
   );
