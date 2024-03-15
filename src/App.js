05fe82d2e2f7d69798cb5766949d98b66da9e8cb
Zorro <152538076+ThembaJNcube@users.noreply.github.com>
2024-03-15 21:22:36 +0000

    Footer completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./components/Navbar";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
