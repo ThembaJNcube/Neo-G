980ad5e22fde9d8a4b9af596465c454c3153bc48
Zorro <152538076+ThembaJNcube@users.noreply.github.com>
2024-03-18 09:24:57 +0000

    Home page made responsive and tailored

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import H1 from "../images/Home/H1.jpg";
 import H2 from "../images/Home/H2.jpg";
+import H3 from "../images/Home/H3.jpg";
 
 function Home() {
   return (
-    <div className="home-container">
+    <div className="section-container">
       <div className="home-content">
         <img src={H1} className="hover:animate-pulse"></img>
       </div>
@@ -16,18 +17,33 @@ function Home() {
             NEO-AGE
           </span>
         </h1>
-        <p>Be part of the movement and engage in the future of training.</p>
+        <p>
+          Be part of the movement and engage in the future of training.{"  "}{" "}
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum culpa
+          dicta ducimus amet delectus cum, vel dolore veniam corrupti doloribus
+          provident, vitae aliquam similique ex.
+        </p>
+      </div>
+      <div className="home-content">
+        <img src={H3} className="hover:animate-pulse"></img>
       </div>
       <div className="home-content writing">
         <h1 className="text-3xl italic font-bold text-[#770707]">NEO-GYM !</h1>
         <p>
           is tailored to accommodate everyone and all their needs, and it is
           here to takeover, avoid being left out and be part of this{"  "}
-          <span className="text-xl text-[#770707]"> MOVEMENT</span>.
+          <span className="text-xl text-[#770707]"> MOVEMENT</span>
+          {"  "}
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          est quos non amet iure rem consectetur assumenda consequuntur vel
+          autem?.
         </p>
       </div>
       <div className="home-content">
         <img src={H2} />
+      </div>
+      <div className="next">
+        <button className="btn" >read more ...</button>
       </div>
     </div>
   );
