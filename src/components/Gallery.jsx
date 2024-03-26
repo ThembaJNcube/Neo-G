f89eb2e7d0ad2c6e839a56ded3a149b78a6a00af
Zorro <152538076+ThembaJNcube@users.noreply.github.com>
2024-03-26 17:33:53 +0000

    Gallery response on 90%

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,28 +4,28 @@ import album from "./album";
 export default function Gallery() {
   return (
     <div className="gallery-container">
-      <div className="gallery-box">
+      <div className="gallery-box hover:animate-pulse">
         <img src={album[0]} alt="gym 1" />
       </div>
-      <div className="gallery-box">
-        <img src={album[1]} alt="gym 1" />
+      <div className="gallery-box hover:animate-bounce">
+        <img src={album[1]} alt="gym 2" />
       </div>
-      <div className="gallery-box">
-        <img src={album[2]} alt="gym 1" />
+      <div className="gallery-box hover:animate-pulse">
+        <img src={album[2]} alt="gym 3" />
       </div>
-      <div className="gallery-box">
-        <img src={album[3]} alt="gym 1" />
+      <div className="gallery-box hover:animate-bounce">
+        <img src={album[3]} alt="gym 4" />
       </div>
-      <div className="gallery-box">
-        <img src={album[4]} alt="gym 1" />
+      <div className="gallery-box hover:animate-pulse">
+        <img src={album[4]} alt="gym 5" />
       </div>
-      <div className="gallery-box">
-        <img src={album[5]} alt="gym 1" />
+      <div className="gallery-box hover:animate-bounce">
+        <img src={album[5]} alt="gym 6" />
       </div>
       <div className="gallery-text">
         <h1>
           This is the{" "}
-          <span className="text-[#721b1b] animate-pulse font-bold">
+          <span className="text-[#bb2929] animate-pulse font-bold">
             Next Level !
           </span>
         </h1>
